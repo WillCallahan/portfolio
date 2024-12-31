d5acb4dd15b947a72c0fcd9c180f6b86d80007cf
William Callahan <willcallaan94@gmail.com>
2024-12-30 23:25:10 -0500

    checkpoint

--- portfolio-web/config/dev.config.js
+++
@@ -1,5 +0,0 @@
-const configuration = {
-	API_URL: JSON.stringify('https://alc7jm51ph.execute-api.us-east-1.amazonaws.com/Prod')
-};
-
-module.exports = configuration;
--- portfolio-web/config/prod.config.js
+++
@@ -1,5 +0,0 @@
-const configuration = {
-	API_URL: JSON.stringify(process.env.API_URL)
-};
-
-module.exports = configuration;
